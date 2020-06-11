b56f2ec23c510bebb1594f2c5829d7de98c48960
Nathan Brown <scottynate05@yahoo.com>
2020-06-10 19:27:09 -0700

    modified App.js function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  // const [date, setDate] = useState(null);
+  // useEffect(() => {
+  //   async function getDate() {
+  //     const res = await fetch('/api/date');
+  //     const newDate = await res.text();
+  //     setDate(newDate);
+  //   }
+  //   getDate();
+  // }, []);
   return (
     <main>
       <h1>Create React App + Go API</h1>
